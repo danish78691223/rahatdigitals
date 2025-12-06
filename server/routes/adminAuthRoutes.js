28415eac03fa5d7c5c4bc9d9e44a03a8be4bc09a
Danish Khan <askdnk2523@gmail.com>
2025-12-06 21:14:17 +0530

    just updated

--- server/routes/adminAuthRoutes.js
+++ server/routes/adminAuthRoutes.js
@@ -1,12 +1,10 @@
 import express from "express";
 import {
-  registerAdmin,
+
   loginAdmin,
 } from "../controllers/adminAuthController.js";
 
 const router = express.Router();
-
-router.post("/register", registerAdmin);
 
 router.post("/login", loginAdmin);
 
